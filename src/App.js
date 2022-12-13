6e566b81f505745d70572930b1a7c24d55568674
deviwlndr <114635284+deviwlndr@users.noreply.github.com>
2022-12-13 12:09:23 +0700

    devi

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="logo1.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Spongebob <code>SquarePants</code> 
         </p>
         <a
           className="App-link"
